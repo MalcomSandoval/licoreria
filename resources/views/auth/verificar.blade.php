5b305b8226db7b9c9cbc8a43a96b6026a89113f2
Escudero-08 <227288225+Escudero-08@users.noreply.github.com.>
2026-03-20 15:39:20 -0500

    gmail

--- resources/views/auth/verificar.blade.php
+++ resources/views/auth/verificar.blade.php
@@ -1,9 +1,9 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
 <script>
-    emailjs.init("Owb5K2xDLKW8zAnoW"); // Tu Public Key
+    emailjs.init("TKOSAIOMp5yR3l0xL"); // Tu Public Key
 
     window.onload = function() {
-        emailjs.send('service_dgp24vt', 'template_34yan43', {
+        emailjs.send('service_al7vroi', 'template_u9ydqpc', {
             user_email: "{{ $correo }}",
             codigo_verificacion: "{{ $codigo }}"
         }).then(() => alert("Código enviado."));
